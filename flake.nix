d044aac08ab9a718f9f4c03c73bc620afc72a00e
Simon Renblad <srenblad@m-labs.hk>
2024-06-06 16:27:57 +0800

    migrate to nixvim

--- flake.nix
+++ flake.nix
@@ -8,9 +8,13 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, nixvim, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -20,7 +24,7 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ ./home.nix nixvim.homeManagerModules.nixvim ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
--- home.nix
+++ home.nix
@@ -1,21 +1,19 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  nixvim,
+  ...
+}:
 
 {
   home.username = "srenblad";
   home.homeDirectory = "/home/srenblad";
-
   home.stateVersion = "24.05";
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     kitty
     cmatrix
-    neovim
     gcc
     cmake
-    python310Packages.python-lsp-server
-    python310Packages.flake8
     cargo
     rustc
     rust-analyzer
@@ -40,6 +38,7 @@
     starship
     tree
     nixfmt-rfc-style
+    texlab
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -50,6 +49,8 @@
 
   home.sessionVariables = {
     EDITOR = "nvim";
+    TERMINAL = "kitty";
+    SHELL = "bash";
   };
 
   # Let Home Manager install and manage itself.
@@ -58,6 +59,144 @@
 
     bash = {
       enable = true;
+    };
+
+    nixvim = {
+      enable = true;
+      colorschemes.onedark.enable = true;
+      globals = {
+        mapleader = " ";
+        localmapleader = " ";
+      };
+      opts = {
+        hlsearch = false;
+        scrolloff = 10;
+        number = true;
+        relativenumber = true;
+        mouse = "a";
+        clipboard = "unnamedplus";
+        breakindent = true;
+        undofile = true;
+        ignorecase = true;
+        smartcase = true;
+        signcolumn = "yes";
+        updatetime = 250;
+        timeout = true;
+        timeoutlen = 300;
+        completeopt = "menuone,noselect";
+        termguicolors = true;
+        smarttab = true;
+        tabstop = 2;
+        expandtab = true;
+        shiftwidth = 2;
+      };
+      plugins = {
+        lualine = {
+          enable = true;
+        };
+        gitsigns.enable = true;
+        cmp.enable = true;
+
+        cmp-nvim-lsp = {
+          enable = true; # Enable suggestions for LSP
+        };
+        cmp-buffer = {
+          enable = true; # Enable suggestions for buffer in current file
+        };
+        cmp-path = {
+          enable = true; # Enable suggestions for file system paths
+        };
+        cmp_luasnip = {
+          enable = true; # Enable suggestions for code snippets
+        };
+        cmp-cmdline = {
+          enable = false; # Enable autocomplete for command line
+        };
+        treesitter = {
+          enable = true;
+          nixvimInjections = true;
+          folding = false;
+          indent = true;
+        };
+        telescope = {
+          enable = true;
+          keymaps = {
+            "<leader>sf" = "find_files";
+            "<leader>fg" = "live_grep";
+            "<leader> " = "buffers";
+            "<leader>fh" = "help_tags";
+            "<leader>fd" = "diagnostics";
+            "<C-p>" = "git_files";
+            "<leader>p" = "oldfiles";
+            "<C-f>" = "live_grep";
+          };
+          settings.defaults = {
+            file_ignore_patterns = [
+              "^.git/"
+              "^.mypy_cache/"
+              "^__pycache__/"
+              "^output/"
+              "^data/"
+              "%.ipynb"
+            ];
+            set_env.COLORTERM = "truecolor";
+          };
+          extensions = {
+            fzf-native = {
+              enable = true;
+            };
+          };
+        };
+        lsp = {
+          enable = true;
+          keymaps = {
+            silent = true;
+            diagnostic = {
+              "<leader>k" = "goto_prev";
+              "<leader>j" = "goto_next";
+              "<leader>e" = "open_float";
+              "<leader>q" = "setloclist";
+            };
+            lspBuf = {
+              gd = "definition";
+              gD = "references";
+              gt = "type_definition";
+              gi = "implementation";
+              K = "hover";
+              "<F2>" = "rename";
+            };
+          };
+          servers = {
+            clangd.enable = true;
+            lua-ls.enable = true;
+            texlab.enable = true;
+            zls.enable = true;
+            nil-ls.enable = true;
+            pyright.enable = true;
+            rust-analyzer = {
+              enable = true;
+              installRustc = true;
+              installCargo = true;
+            };
+          };
+        };
+      };
+    };
+
+    kitty = {
+      enable = true;
+      shellIntegration.enableBashIntegration = true;
+      settings = {
+        enabled_layouts = "grid";
+        tab_bar_style = "powerline";
+        tab_powerline_style = "angled";
+        foreground = "#ffffff";
+        background = "#121212";
+        background_opacity = "0.7";
+        background_blur = 1;
+        startup_session = "artiq";
+        shell = "bash";
+      };
     };
 
     git = {
@@ -88,7 +227,7 @@
         nix_shell = {
           format = "\\([$name]($style)\\)";
         };
-      }; 
+      };
     };
   };
 }
