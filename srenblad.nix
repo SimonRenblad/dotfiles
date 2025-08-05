541bb1d0c0856219a17d5b7fb85d48419c50e21a
Simon Renblad <srenblad@m-labs.hk>
2025-08-05 14:55:09 +0800

    rename home.nix -> srenblad.nix

---
+++ srenblad.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  home.username = "srenblad";
+  home.homeDirectory = "/home/srenblad";
+  home.stateVersion = "24.05";
+  home.sessionVariables = {
+    EDITOR = "hx";
+    TERMINAL = "alacritty";
+    SHELL = "bash";
+    QT_PLUGIN_PATH = "${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.dev.qtPluginPrefix}:${pkgs.qt5.qtsvg.bin}/${pkgs.qt5.qtbase.dev.qtPluginPrefix}";
+  };
+}
