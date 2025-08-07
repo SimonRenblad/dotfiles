f3699794173c883ac4e3aca47f6d5ab3c7f037d8
Simon Renblad <srenblad@m-labs.hk>
2025-08-07 17:52:56 +0800

    bash: only initialize starship in interactive terminal

--- programs/bash.nix
+++ programs/bash.nix
@@ -3,8 +3,10 @@
     enable = true;
     enableCompletion = true;
     bashrcExtra = ''
-      eval "$(fzf --bash)"
-      eval "$(starship init bash)"  
+      if [[ -t 1 ]]; then
+        eval "$(fzf --bash)"
+        eval "$(starship init bash)"  
+      fi
     '';
   };
 }
