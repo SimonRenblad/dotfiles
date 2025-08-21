642f5d8a54d0c50d3208fbdcd13a0f4386bb5775
Simon Renblad <srenblad@m-labs.hk>
2025-08-21 17:55:25 +0800

    cut down on tmux launch script

--- launch_tmux.sh
+++ launch_tmux.sh
@@ -11,33 +11,15 @@ then
 
     tmux rename-window -t 0 'artiq'
     tmux split-window -t 0 -h
-    tmux split-window -t 1 -v -l "33%"
-    tmux split-window -t 1 -v -l "50%"
     tmux send-keys -t 0 'cd ~/artiq; $EDITOR .' C-m
     tmux send-keys -t 1 'cd ~/artiq; nix develop' C-m
-    tmux send-keys -t 2 'cd ~/artiq; clear' C-m
-    tmux send-keys -t 3 'cd ~/artiq; clear' C-m
     tmux selectp -t 0
 
     tmux new-window -t $SESSION:1 -n 'artiq-zynq'
     tmux split-window -t 0 -h
-    tmux split-window -t 1 -v -l "33%"
-    tmux split-window -t 1 -v -l "50%"
     tmux send-keys -t 0 'cd ~/artiq-zynq; $EDITOR .' C-m
-    tmux send-keys -t 1 'cd ~/zynq-rs; nix develop' C-m
-    tmux send-keys -t 2 'cd ~/artiq-zynq; nix develop' C-m
-    tmux send-keys -t 3 'cd ~/artiq-zynq; clear' C-m
-    tmux selectp -t 0
-    
-    tmux new-window -t $SESSION:2 -n 'nac3'
-    tmux split-window -t 0 -h
-    tmux split-window -t 1 -v -l "33%"
-    tmux split-window -t 1 -v -l "50%"
-    tmux send-keys -t 0 'cd ~/nac3; $EDITOR .' C-m
-    tmux send-keys -t 1 'cd ~/nac3; clear' C-m
-    tmux send-keys -t 2 'cd ~/nac3; clear' C-m
-    tmux send-keys -t 3 'cd ~/nac3; clear' C-m
-    tmux selectp -t 0
+    tmux send-keys -t 1 'cd ~/artiq-zynq; nix develop' C-m
+    tmux selectp -t 0   
 fi
 
 # Attach Session, on the Main window
