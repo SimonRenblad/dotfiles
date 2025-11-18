3d3b49f57d6bfb43655894043f31d18824c18f52
Simon Renblad <srenblad@m-labs.hk>
2025-11-18 18:41:50 +0800

    beginning to readd nixvim (wip)

--- flake.nix
+++ flake.nix
@@ -32,6 +32,10 @@
       url = "github:SimonRenblad/pymenth";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
   };
 
   outputs =
